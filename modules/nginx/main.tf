85f50f2833a3d39e1a988f948b9ae23622a74396
Alex Darigan <alexdarigan@VFIEVOX3.Router>
2025-09-09 17:20:33 +0100

    seperate into apps and infra modules

---
+++ modules/nginx/main.tf
@@ -0,0 +1,70 @@
+terraform {
+  required_providers {
+    kubernetes = {
+      source = "hashicorp/kubernetes"
+      version = "~> 2.23" # Use a compatible version
+    }
+  }
+}
+
+provider "kubernetes" {
+  config_path = var.kubeconfig
+}
+
+// Web Page
+resource "kubernetes_deployment_v1" "nginx_deployment" {
+  metadata {
+    name = var.deployment.name
+    labels = {
+      app = var.app_name
+    }
+  }
+  spec {
+    replicas = var.deployment.replicas
+    selector {
+      match_labels = {
+        app = var.app_name
+      }
+    }
+    template {
+      metadata {
+        labels = {
+          app = var.app_name
+        }
+      }
+      spec {
+        container {
+          name = var.container.name
+          image = var.container.image
+          port {
+            container_port = var.container.port
+          }
+        }
+      }
+    }
+  }
+}
+
+resource "kubernetes_service_v1" "nginx_service" {
+  metadata {
+    name = var.service.name
+    labels = {
+      app = var.app_name
+    }
+  }
+  spec {
+    selector = {
+      app = var.app_name
+    }
+    port {
+      protocol = var.service.protocol
+      port = var.service.port
+      target_port = var.service.target_port
+    }
+    type = var.service.type
+  }
+}
+
+output "service_name" {
+  value = kubernetes_service_v1.nginx_service.metadata[0].name
+}
